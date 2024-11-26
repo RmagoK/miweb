dc6a2556681caea17c3d2051ca4acf3df9ca8728
Rosheed Mago <rmagokassie@gmail.com>
2024-11-26 11:14:54 -0400

    Agregando valores a Lista_de_valores

--- modulo_prueba.py
+++ modulo_prueba.py
@@ -1,3 +1,5 @@
-Lista_de_Valores = ["Vijay", "Savir", "Rosheed", True]
+Lista_de_Valores = ["prueba1", "prueba2", "prueba3"]
+A, B, C = Lista_de_Valores
 for lista in Lista_de_Valores:
-    print(f"Los valores de los datos: {lista}")
+    print(f"Los valores de los datos: {lista} {A}")
+    
